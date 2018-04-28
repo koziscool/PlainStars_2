eee756c590bd74569047fe46ecec55beef7b7f11
John Kosmicke <kozjob@yahoo.com>
2018-04-27 22:21:26 -0700

    update

--- index.js
+++ index.js
@@ -3,23 +3,29 @@
 const Card = (props) => {
   return(
     <div style={{ margin: '1em' }}>
-      <img width="100" src="https://avatars1.githubusercontent.com/u/1393010?v=4" />
+      <img width="100" src={ props.avatar_url } />
       <div style={{ display: 'inline-block', marginLeft: 10 }}>
-        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>John Kosmicke</div>
-        <div>tetris</div>
+        <div style={{ fontSize: '1.25em', fontWeight: 'bold' }}>{ props.name }</div>
+        <div>{ props.company }</div>
       </div>
     </div>
   );
 }
 
+
+let data=[
+  {
+    name: "John Kosmicke",
+    company: "tetris",
+    avatar_url: "https://avatars1.githubusercontent.com/u/1393010?v=4",
+  },
+]
+
 const CardList = (props) => {
   return(
     <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      { props.cards.map( card => <Card  {...card} /> ) }
+
     </div>
   );  
 }
@@ -27,7 +33,7 @@ const CardList = (props) => {
 
 
 ReactDOM.render(
-  <CardList />,
+  <CardList cards={ data } />,
   document.getElementById('root')
 );
 
