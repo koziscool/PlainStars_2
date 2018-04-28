4a4494cb21160419e16d35c8ab349effbf170c57
John Kosmicke <kozjob@yahoo.com>
2018-04-27 19:27:53 -0700

    update

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ class Button extends React.Component {
   state = { counter: 0 };
 
   handleClick = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
+    this.setState( (prevState) => ({
+      counter: prevState.counter + 1,
+    }));
   };
 
   render() {
