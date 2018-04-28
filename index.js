45409c1b6286e9a9db44fef413fa036f39d3ce3d
John Kosmicke <kozjob@yahoo.com>
2018-04-27 19:26:00 -0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
 
-const Button =  function (props) {
-  return (
-    <button>Go</button>
-  );
+class Button extends React.Component {
+
+  state = { counter: 0 };
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  };
+
+  render() {
+    return (
+      <button  onClick={ this.handleClick } >{ this.state.counter }</button>
+    );
+  }
 };
 
 ReactDOM.render(
