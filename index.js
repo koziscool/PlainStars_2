e112b983c367a926600aa69826991a32ac430ab0
John Kosmicke <kozjob@yahoo.com>
2018-04-28 22:34:52 -0700

    update

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ const Answer = (props) => {
   return(
     <div className="col-5">
       { props.selectedNumbers.map( (number, i) =>
-        <span key={i}>{number}</span>
+        <span key={i}  onClick={ ()=> props.unselectNumber(number) }>
+          {number}
+        </span>
       )}
     </div>
   );  
@@ -64,7 +66,13 @@ class Game extends React.Component {
     this.setState( prevState => ({
       selectedNumbers: prevState.selectedNumbers.concat(clickedNumber),
     }));
-  }
+  };
+
+  unselectNumber = (clickedNumber) => {
+    this.setState( prevState => ({
+      selectedNumbers: prevState.selectedNumbers.filter(number => number !== clickedNumber),
+    }));
+  };
 
   render() {
     return(
@@ -74,7 +82,8 @@ class Game extends React.Component {
         <div className="row">
           <Stars numberOfStars={ this.state.numberOfStars }/>
           <Button />
-          <Answer selectedNumbers= { this.state.selectedNumbers } />
+          <Answer selectedNumbers= { this.state.selectedNumbers } 
+                        unselectNumber = { this.unselectNumber }/>
         </div>
         <br />
         <Numbers selectedNumbers= { this.state.selectedNumbers }
