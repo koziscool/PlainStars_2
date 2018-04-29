dde5d14634ee8e074c678534f8791b4ed131024e
John Kosmicke <kozjob@yahoo.com>
2018-04-28 21:55:43 -0700

    update

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 
 const Stars = (props) => {
+
+  const numberOfStars = 1 + Math.floor( Math.random() * 9 );
+  let stars = [];
+  for( var i = 0; i < numberOfStars ; i++ ) {
+    stars.push( <i key={i} className="fa fa-star"></i> );
+  }
+    
+
   return(
     <div className="col-5">
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
-      <i className="fa fa-star"></i>
+      { stars }
     </div>
   );  
 };
